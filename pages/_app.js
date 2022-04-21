2c6fe31ffa8e0f96f63fd95e018d01ea8f18026e
Evans Ding <evansding@gmail.com>
2022-04-20 17:22:06 -0700

    changed font

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import '../styles/index.scss'
+import Head from "next/head";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400&display=swap" rel="stylesheet"/>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 
